db47344d1b408b6db7160079795d78a9a56b262b
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-04-02 17:54:34 -0500

    View NothingSelected

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,15 +1,16 @@
 import { MailOutline } from "@mui/icons-material"
-import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
   return (
     <>
       <JournalLayout>
-        <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod iure dolor laborum accusantium alias iste, perferendis id obcaecati repellat commodi facilis at nostrum vitae vel ipsa earum consequuntur consequatur soluta!</Typography>
+        {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod iure dolor laborum accusantium alias iste, perferendis id obcaecati repellat commodi facilis at nostrum vitae vel ipsa earum consequuntur consequatur soluta!</Typography> */}
 
         {/* Nothing selected */}
-
+        <NothingSelectedView/>
+        
         {/* NoteView */}
 
       </JournalLayout>
