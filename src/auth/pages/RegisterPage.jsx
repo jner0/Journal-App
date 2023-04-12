6f60239a2c78883f0e01acd608df6b624740daaf
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-04-12 17:17:39 -0500

    Formulario de registro

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,61 +1,81 @@
-import { Link as RouterLink } from 'react-router-dom';
-import { Google } from "@mui/icons-material"
-import { Button, Grid, Link, TextField, Typography } from "@mui/material"
-import { AuthLayout } from '../layout/AuthLayout';
+import { Link as RouterLink } from "react-router-dom";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { AuthLayout } from "../layout/AuthLayout";
+import { useForm } from "../../hooks";
 
 export const RegisterPage = () => {
+  const formData = {
+    email: "josenemecio@gmail.com",
+    password: "123456",
+    displayName: "Nemecio Rodriguez",
+  };
+
+  const { displayName, email, password, onInputChange, formState } =
+    useForm(formData);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(formState);
+  };
+
   return (
-
-     <AuthLayout title='Crear Cuenta'>  {/* El AuthLayout funciona para todos los elementos que se vana  reutilizar */}
-      <form>
-          <Grid container>
-            <Grid item xs={12} sx={{mt: 2}}>
-              <TextField 
-                label="Nombre Completo" 
-                type="text" 
-                placeholder="Nombre Completo" 
-                fullWidth 
-              />
-            </Grid>
-
-            <Grid item xs={12} sx={{mt: 2}}>
-              <TextField 
-                label="Correo" 
-                type="email" 
-                placeholder="correo@gmail.com" 
-                fullWidth 
-              />
-            </Grid>
-
-            <Grid item xs={12} sx={{mt: 2}}> 
-              <TextField 
-                label="Contraseña" 
-                type="password" 
-                placeholder="Contraseña" 
-                fullWidth 
-              />              
-            </Grid>
-
-            <Grid container spacing={ 2 } sx={{ mb: 2, m: 1 }}>
-              <Grid item xs={ 12 } >
-                <Button variant="contained" fullWidth>
-                  Crear Cuenta
-                </Button>
-              </Grid>
-            </Grid>
-          
-
-            <Grid container direction='row' justifyContent='end'>
-              <Typography sx={{mr: 1}} >¿Ya tienes cuenta?</Typography>
-              <Link component={ RouterLink } color="inherit" to="/auth/login">
-                Ingresar
-              </Link>
-            </Grid>
-            
+    <AuthLayout title="Crear Cuenta">
+      {" "}
+      {/* El AuthLayout funciona para todos los elementos que se vana  reutilizar */}
+      <form onSubmit={onSubmit}>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Nombre Completo"
+              type="text"
+              placeholder="Nombre Completo"
+              fullWidth
+              name="displayName"
+              value={displayName}
+              onChange={onInputChange}
+            />
           </Grid>
 
-        </form>
-    </AuthLayout>
-  )
-}
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Correo"
+              type="email"
+              placeholder="correo@gmail.com"
+              fullWidth
+              name="email"
+              value={email}
+              onChange={onInputChange}
+            />
+          </Grid>
 
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Contraseña"
+              type="password"
+              placeholder="Contraseña"
+              fullWidth
+              name="password"
+              value={password}
+              onChange={onInputChange}
+            />
+          </Grid>
+
+          <Grid container spacing={2} sx={{ mb: 2, m: 1 }}>
+            <Grid item xs={12}>
+              <Button variant="contained" fullWidth type="submit">
+                Crear Cuenta
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" justifyContent="end">
+            <Typography sx={{ mr: 1 }}>¿Ya tienes cuenta?</Typography>
+            <Link component={RouterLink} color="inherit" to="/auth/login">
+              Ingresar
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
+};
