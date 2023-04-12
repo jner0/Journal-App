c38fce193352bfb9bee2e64bfdaf5a37acf542c6
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-04-11 21:01:36 -0500

    Manejo de Formulario Login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,57 +1,85 @@
-import { Link as RouterLink } from 'react-router-dom';
-import { Google } from "@mui/icons-material"
-import { Button, Grid, Link, TextField, Typography } from "@mui/material"
-import { AuthLayout } from '../layout/AuthLayout';
+import { Link as RouterLink } from "react-router-dom";
+import { Google } from "@mui/icons-material";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { AuthLayout } from "../layout/AuthLayout";
+import { useForm } from "../../hooks/useForm";
+import { useDispatch } from "react-redux";
+import {
+  checkingAuthentication,
+  startGoogleSignIn,
+} from "../../store/auth/thunks";
 
 export const LoginPage = () => {
+  const dispatch = useDispatch();
+  const { email, password, onInputChange } = useForm({
+    email: "josenemecio@hotmail.com",
+    password: "123456",
+  });
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    console.log({ email, password });
+    dispatch(checkingAuthentication());
+  };
+
+  const onGoogleSignIn = () => {
+    console.log("onGoogleSignIn");
+    dispatch(startGoogleSignIn());
+  };
+
   return (
-
-     <AuthLayout title='Login'>  {/* El AuthLayout funciona para todos los elementos que se vana  reutilizar */}
-      <form>
-          <Grid container>
-            <Grid item xs={12} sx={{mt: 2}}>
-              <TextField 
-                label="Correo" 
-                type="email" 
-                placeholder="correo@gmail.com" 
-                fullWidth 
-              />
-            </Grid>
-
-            <Grid item xs={12} sx={{mt: 2}}> 
-              <TextField 
-                label="Contraseña" 
-                type="password" 
-                placeholder="Contraseña" 
-                fullWidth 
-              />              
-            </Grid>
-
-            <Grid container spacing={ 2 } sx={{ mb: 2, m: 1 }}>
-              <Grid item xs={ 12 } sm={ 6 }>
-                <Button variant="contained" fullWidth>
-                  Login
-                </Button>
-              </Grid>
-
-              <Grid item xs={ 12 } sm={ 6 }>
-                <Button variant="contained" fullWidth>
-                  <Google/>
-                  <Typography sx={{ ml: 1 }} >Google</Typography>
-                </Button>
-              </Grid>
-            </Grid>
-          
-
-            <Grid container direction='row' justifyContent='end'>
-              <Link component={ RouterLink } color="inherit" to="/auth/register">
-                Crear una cuenta
-              </Link>
-            </Grid>
-            
+    <AuthLayout title="Login">
+      {" "}
+      {/* El AuthLayout funciona para todos los elementos que se vana  reutilizar */}
+      <form onSubmit={onSubmit}>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Correo"
+              type="email"
+              placeholder="correo@gmail.com"
+              fullWidth
+              name="email"
+              value={email}
+              onChange={onInputChange}
+            />
           </Grid>
 
-        </form>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Contraseña"
+              type="password"
+              placeholder="Contraseña"
+              fullWidth
+              name="password"
+              value={password}
+              onChange={onInputChange}
+            />
+          </Grid>
+
+          <Grid container spacing={2} sx={{ mb: 2, m: 1 }}>
+            <Grid item xs={12} sm={6}>
+              <Button type="submit" variant="contained" fullWidth>
+                Login
+              </Button>
+            </Grid>
+
+            <Grid item xs={12} sm={6}>
+              <Button onClick={onGoogleSignIn} variant="contained" fullWidth>
+                <Google />
+                <Typography sx={{ ml: 1 }}>Google</Typography>
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction="row" justifyContent="end">
+            <Link component={RouterLink} color="inherit" to="/auth/register">
+              Crear una cuenta
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
     </AuthLayout>
-  )
-}
+  );
+};
---
+++ src/store/auth/thunks.js
@@ -0,0 +1,14 @@
+// Acciones que puedo despechar pero que tienen una tarea asincrona
+import { checkingCredentials } from "./authSlice";
+
+export const checkingAuthentication = (email, password) => {
+  return async (dispatch) => {
+    dispatch(checkingCredentials());
+  };
+};
+
+export const startGoogleSignIn = () => {
+  return async (dispatch) => {
+    dispatch(checkingCredentials());
+  };
+};
