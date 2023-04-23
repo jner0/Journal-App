b72b2ad3c70c57a3a011555ed30b3e25675f960b
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-04-23 16:14:38 -0500

    Menu lateral de notas

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,10 +1,11 @@
-import { TurnedInNot } from "@mui/icons-material"
 import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material"
 import { useSelector } from "react-redux"
+import { SideBarItem } from "./";
 
 export const SideBar = ({drawerWidth = 240}) => {
 
-   const { displayName } =  useSelector( state => state.auth)
+   const { displayName } =  useSelector( state => state.auth);
+   const { notes } = useSelector( state => state.journal );
     
   return (
     <Box
@@ -29,24 +30,14 @@ export const SideBar = ({drawerWidth = 240}) => {
 
             <List>
                 {
-                    [ 'Enero', 'Febrero', 'Marzo', 'Abril' ].map( text => (
-                        <ListItem key={ text } disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <TurnedInNot></TurnedInNot>
-                                </ListItemIcon>
-                                <Grid container>
-                                    <ListItemText primary={ text } />
-                                    <ListItemText secondary={ 'lalalalalalalalalalal batman digo liderr' } />
-                                </Grid>
-                            </ListItemButton>
-                        </ListItem> 
+                    notes.map( note => (
+                        <SideBarItem key={ note.id } {...note}/>
                     ))
                 }
             </List>
 
         </Drawer>
 
-    </Box>
+    </Box>          
   )
 }
