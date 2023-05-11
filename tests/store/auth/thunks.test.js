b06c90b545e3fdcf048e74b4cd28d9028695d05e
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-05-11 18:27:18 -0500

    Pruebas en startLogin

--- tests/store/auth/thunks.test.js
+++ tests/store/auth/thunks.test.js
@@ -1,6 +1,7 @@
-import { singInWithGoogle } from "../../../src/firebase/providers";
+import { loginWithEmailPassword, logoutFirebase, singInWithGoogle } from "../../../src/firebase/providers";
 import { checkingCredentials, login, logout } from "../../../src/store/auth/authSlice";
-import { checkingAuthentication, startGoogleSignIn } from "../../../src/store/auth/thunks";
+import { checkingAuthentication, startGoogleSignIn, startLoginWithEmailPassword, startLogout } from "../../../src/store/auth/thunks";
+import { clearNotesLogout } from "../../../src/store/journal/journalSlice";
 import { demoUser } from "../../fixtures/authFixtures";
 
 jest.mock('../../../src/firebase/providers');
@@ -40,8 +41,32 @@ describe('pruebas en AuthThunks', () => {
         await startGoogleSignIn()(dispatch);
         
         expect( dispatch ).toHaveBeenCalledWith(checkingCredentials());
-        expect( dispatch ).toHaveBeenCalledWith(checkingCredentials( logout ));
+        expect( dispatch ).toHaveBeenCalledWith(( logout(loginData.errorMessage) ));
         
+
+    });
+
+    test('startLoginWithEmailAndPassword debe de llamar checkingCredentials y Login - Exito', async() => { 
+
+        const loginData = { ok: true, ...demoUser };
+        const formData = { email: demoUser.email, password: '123456' };
+
+        await loginWithEmailPassword.mockResolvedValue( loginData );
+
+        await startLoginWithEmailPassword(formData)(dispatch);
+
+        expect( dispatch ).toHaveBeenCalledWith(checkingCredentials());
+        expect( dispatch ).toHaveBeenCalledWith( login(loginData));
+
+    });
+
+    test('startLogout debe de llamar logoutFirebase, clearnotes y logout', async() => { 
+
+        await startLogout()(dispatch);
+
+        expect( logoutFirebase ).toHaveBeenCalled();
+        expect( dispatch ).toHaveBeenCalledWith( clearNotesLogout() );
+        expect( dispatch ).toHaveBeenCalledWith( logout() );
 
     });
 
